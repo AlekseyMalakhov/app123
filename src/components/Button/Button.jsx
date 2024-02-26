029f7a75da8b64b7432214c555e322e4f0b85180
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-02-26 18:06:34 +0300

    add counter

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
 import classes from "./Button.module.css";
 
-export default function Button({ shot, setShot }) {
+export default function Button({ setShot }) {
+    const [count, setCount] = useState(5);
+    const [waiting, setWaiting] = useState(false);
+
+    useEffect(() => {
+        if (waiting) {
+            let timer;
+            if (count > 0) {
+                timer = setTimeout(() => setCount(count - 1), 1000);
+            } else {
+                setWaiting(false);
+                setCount(5);
+                clearTimeout(timer);
+                setShot(false);
+            }
+        }
+    }, [waiting, count]);
+
+    const start = () => {
+        setShot(true);
+        setWaiting(true);
+    };
+
     return (
-        <div className={classes.button} onClick={() => setShot(true)}>
-            <div className={classes.text}>{shot ? 123 : "Start"}</div>
+        <div className={classes.button} onClick={start}>
+            <div className={classes.text}>{waiting ? count : "Start"}</div>
         </div>
     );
 }
