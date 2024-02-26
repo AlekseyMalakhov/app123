747d93477dee0bce905270df5a8242a7451af081
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-02-26 12:23:49 +0300

    some moving

--- src/components/Circle/Circle.jsx
+++ src/components/Circle/Circle.jsx
@@ -1,5 +1,5 @@
 import classes from "./Circle.module.css";
 
 export default function Circle({ shot }) {
-    return <div className={`${classes.circle} ${shot ? classes.shot : classes.init}`}></div>;
+    return <div className={`${classes.circle} ${shot ? "" : classes.moving} ${shot ? classes.shot : classes.init}`}></div>;
 }
